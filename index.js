78599554ed66fc7d0d1a1841bbcf68577fbf67ca
Andy Sylvester <andy@andysylvester.com>
2024-07-08 15:26:24 -0700

    Update index.js
    Added debug code and error-proofing code

--- index.js
+++ index.js
@@ -8,19 +8,33 @@ async function fetchFeedTitle(xmlUrl) {
     const response = await axios.get(xmlUrl);
     const xmlData = response.data;
     let title = '';
+	let info = { title: '', site: ''};
 
-    // Parse XML to extract title based on feed type
+    // Parse XML to extract title based on feed type and extract site URL based on feed type
     parseString(xmlData, (err, result) => {
       if (err) throw err;
 
       if (result.rss && result.rss.channel && result.rss.channel[0].title) {
-        title = result.rss.channel[0].title[0];
+        info.title = result.rss.channel[0].title[0];
       } else if (result.feed && result.feed.title) {
-        title = result.feed.title[0];
+        info.title = result.feed.title[0]._;
+        console.log("Title Atom:",result.feed.title[0]._) 
       }
-    });
+ 
+      if (result.rss && result.rss.channel && result.rss.channel[0].link[0]) {
+        info.site = result.rss.channel[0].link[0];
+        console.log("Channel:",result.rss.channel[0].link[0]) 
+      } else if (result.feed && result.feed.link) {
+        info.site = result.feed.link[2].$.href;
+        console.log("ChannelAtom:",result.feed.link[2].$.href) 
+      }
+      console.log("Title:", info.title);
+      console.log("Site:", info.site);
+	  
 
-    return title;
+ });
+
+    return info;
   } catch (error) {
     console.error(`Error fetching ${xmlUrl}: ${error.message}`);
     return null;
@@ -39,15 +53,32 @@ async function generateOPML(urls) {
   for (const url of urls) {
     const xmlUrl = url.trim();
     const htmlUrl = xmlUrl; // Assuming htmlUrl is same as xmlUrl
+	feedinfo = { title: '', site: ''};
+	let site = '';
+	let title = '';
 
-    // Fetch feed title
-    const title = await fetchFeedTitle(xmlUrl);
+    // Fetch feed title and site URL
+    feedinfo = await fetchFeedTitle(xmlUrl);
+	
+    console.log("Null test:", feedinfo);
+	if (feedinfo === null)
+	{
+		site = xmlUrl;
+		title = xmlUrl;
+        console.log("Null test:", site, title);
+	}
+	else
+	{
+		site = feedinfo.site;
+		title = feedinfo.title;
+        console.log("Null test feedinfo:", site, title);
+	}	
 
     // Append to OPML
-    opml += `    <outline type="rss" htmlUrl="${htmlUrl}" xmlUrl="${xmlUrl}" text="${title || xmlUrl}"/>\n`;
+    opml += `    <outline type="rss" text="${title || xmlUrl}" htmlUrl="${site || xmlUrl}" xmlUrl="${xmlUrl}"/>\n`;
   }
 
-  opml += `  </body>
+  opml += `  </body>"
 </opml>`;
 
   // Write OPML to file
